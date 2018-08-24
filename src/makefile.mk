4345476f6cae52575740ea8b1dc37950a0c130f1
Zachary Wassall <runer112@gmail.com>
2018-08-24 11:12:11 -0700

    Almost works

--- src/makefile.mk
+++ src/makefile.mk
@@ -35,6 +35,8 @@ ICONPNG ?= $(ICON)
 DEBUGMODE = NDEBUG
 CCDEBUGFLAG = -nodebug
 
+UNIXPATH = $(subst \,/,$(1))
+
 # get the os specific items
 ifeq ($(OS),Windows_NT)
 SHELL     := cmd.exe
@@ -56,7 +58,7 @@ RMDIR      = call && (if exist $(1) rmdir /s /q $(1))
 MKDIR      = call && (if not exist $(1) mkdir $(1))
 else
 MAKEDIR   := $(CURDIR)
-NATIVEPATH = $(subst \,/,$(1))
+NATIVEPATH = $(UNIXPATH)
 WINPATH    = $(subst \,\\,$(shell winepath -w $(1)))
 WINRELPATH = $(subst /,\,$(1))
 RM         = rm -f
@@ -79,49 +81,62 @@ BINDIR := $(call NATIVEPATH,$(BINDIR))
 GFXDIR := $(call NATIVEPATH,$(GFXDIR))
 
 # generate default names
-TARGETBIN     := $(TARGET).bin
-TARGETMAP     := $(TARGET).map
-TARGETTYPE    := $(TARGET).8xp
-ICON_ASM      := iconc.src
+TARGETBIN := $(TARGET).bin
+TARGETMAP := $(TARGET).map
+TARGET8XP := $(TARGET).8xp
+ICON_ASM  := iconc.src
 
 # init conditionals
-F_STARTUP     := $(call NATIVEPATH,$(CEDEV)/lib/cstartup.src)
-F_LAUNCHER    := $(call NATIVEPATH,$(CEDEV)/lib/libheader.src)
-F_CLEANUP     := $(call NATIVEPATH,$(CEDEV)/lib/ccleanup.src)
-F_ICON        := $(OBJDIR)/$(ICON_ASM)
+F_STARTUP    := $(call NATIVEPATH,$(CEDEV)/lib/cstartup.src)
+F_LAUNCHER   := $(call NATIVEPATH,$(CEDEV)/lib/libheader.src)
+F_CLEANUP    := $(call NATIVEPATH,$(CEDEV)/lib/ccleanup.src)
+F_ICON       := $(OBJDIR)/$(ICON_ASM)
+F_CONVPNGINI := $(GFXDIR)/convpng.ini
 
 # set use cases
 U_CLEANUP = 0
 U_ICON    = 0
 
 # source: http://blog.jgc.org/2011/07/gnu-make-recursive-wildcard-function.html
-rwildcard = $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2)$(filter $(subst *,%,$2),$d))
+rwildcard = $(foreach d,$(wildcard $(call UNIXPATH,$1*)),$(call rwildcard,$d/,$2)$(filter $(subst *,%,$2),$d))
 
-# find all of the available C, H and ASM files (Remember, you can create C <-> assembly routines easily this way)
-CSOURCES      := $(call NATIVEPATH,$(call rwildcard,$(SRCDIR),*.c))
-CPPSOURCES    := $(call NATIVEPATH,$(call rwildcard,$(SRCDIR),*.cpp))
-USERHEADERS   := $(call NATIVEPATH,$(call rwildcard,$(SRCDIR),*.h))
-USERHEADERS   += $(call NATIVEPATH,$(call rwildcard,$(SRCDIR),*.hpp))
-ASMSOURCES    := $(call NATIVEPATH,$(call rwildcard,$(SRCDIR),*.asm))
+# find all of the available C, H, and ASM files
+CSOURCES    := $(call NATIVEPATH,$(call rwildcard,$(SRCDIR),*.c))
+CPPSOURCES  := $(call NATIVEPATH,$(call rwildcard,$(SRCDIR),*.cpp))
+USERHEADERS := $(call NATIVEPATH,$(call rwildcard,$(SRCDIR),*.h))
+USERHEADERS += $(call NATIVEPATH,$(call rwildcard,$(SRCDIR),*.hpp))
+ASMSOURCES  := $(call NATIVEPATH,$(call rwildcard,$(SRCDIR),*.asm))
 
 # create links for later
-LINK_CSOURCES := $(filter %.src,$(patsubst $(SRCDIR)/%.c,$(OBJDIR)/%.src,$(subst \,/,$(CSOURCES))))
+LINK_CSOURCES  := $(filter %.src,$(patsubst $(SRCDIR)/%.c,$(OBJDIR)/%.src,$(subst \,/,$(CSOURCES))))
 LINK_CPPSOURCES := $(filter %.src,$(patsubst $(SRCDIR)/%.cpp,$(OBJDIR)/%.src,$(subst \,/,$(CSPPOURCES))))
 LINK_ASMSOURCES := $(ASMSOURCES)
 
 # files created to be used for linking
-LINK_FILES    += $(LINK_CSOURCES)
-LINK_FILES    += $(LINK_CPPSOURCES)
-LINK_FILES    += $(LINK_ASMSOURCES)
-LINK_FILES    += $(call NATIVEPATH,$(wildcard $(CEDEV)/lib/shared/*.src))
-LINK_FILES    += $(call NATIVEPATH,$(wildcard $(CEDEV)/lib/fileio/*.src))
-LINK_LIBLOAD  := $(call NATIVEPATH,$(wildcard $(CEDEV)/lib/libload/*.asm))
+LINK_FILES   += $(LINK_CSOURCES)
+LINK_FILES   += $(LINK_CPPSOURCES)
+LINK_FILES   += $(LINK_ASMSOURCES)
+LINK_FILES   += $(call NATIVEPATH,$(wildcard $(CEDEV)/lib/shared/*.src))
+LINK_FILES   += $(call NATIVEPATH,$(wildcard $(CEDEV)/lib/fileio/*.src))
+LINK_LIBLOAD := $(call NATIVEPATH,$(wildcard $(CEDEV)/lib/libload/*.asm))
 
 # check if there is an icon present that we can convert; if so, generate a recipe to build it properly
 ifneq ("$(wildcard $(ICONPNG))","")
-LINK_ICON  := $(F_ICON)
-ICON_CONV  := $(PG) -c $(ICONPNG)$(comma)$(call NATIVEPATH,$(LINK_ICON))$(comma)$(DESCRIPTION)
+LINK_ICON := $(F_ICON)
 U_ICON     = 1
+endif
+
+# find all of the generated graphics files
+GFXLOG     := $(call NATIVEPATH,$(GFXDIR)/convpng.log)
+GFXSOURCES := $(GFXLOG)
+GFXSOURCES += $(call NATIVEPATH,$(call rwildcard,$(GFXDIR),*.c))
+GFXSOURCES += $(call NATIVEPATH,$(call rwildcard,$(GFXDIR),*.h))
+GFXSOURCES += $(call NATIVEPATH,$(call rwildcard,$(GFXDIR),*.asm))
+
+# check if there are graphics present that we can convert; if so, generate a recipe to build them properly
+ifneq ("$(wildcard $(F_CONVPNGINI))","")
+PNGSOURCES := $(call rwildcard,$(GFXDIR),*.png)
+TARGETGFX  := $(GFXLOG)
 endif
 
 # determine if output should be archived or compressed
@@ -146,15 +161,15 @@ LINK_FILES += $(wildcard $(CEDEV)/lib/static/*.src)
 endif
 
 # define the nesassary headers, along with any the user may have defined, where modification should just trigger a build
-HEADERS :=$(strip $(subst $(space),;,$(call WINPATH,$(sort $(dir $(USERHEADERS))))))
+HEADERS := $(strip $(subst $(space),;,$(call WINPATH,$(sort $(dir $(USERHEADERS))))))
 ifeq ($(words $(HEADERS)),0)
-HEADERS :=$(call WINPATH,$(CEDEV)/include);$(call WINPATH,$(CEDEV)/include/compat)
+HEADERS := $(call WINPATH,$(CEDEV)/include);$(call WINPATH,$(CEDEV)/include/compat)
 else
-HEADERS +=$(strip ;$(call WINPATH,$(CEDEV)/include);$(call WINPATH,$(CEDEV)/include/compat))
+HEADERS += $(strip ;$(call WINPATH,$(CEDEV)/include);$(call WINPATH,$(CEDEV)/include/compat))
 endif
-HEADERS :=$(subst \;,;,$(HEADERS))
-HEADERS :=$(subst \;,;,$(HEADERS))
-HEADERS :=$(subst /;,;,$(HEADERS))
+HEADERS := $(subst \;,;,$(HEADERS))
+HEADERS := $(subst \;,;,$(HEADERS))
+HEADERS := $(subst /;,;,$(HEADERS))
 
 # define the C flags used by the Zilog compiler
 CFLAGS ?= \
@@ -179,52 +194,63 @@ LDFLAGS ?= \
 	-i 'sources '$(F_STARTUP)'' \
 	-i 'deps $(call NATIVEPATH,$(LINK_FILES))'
 
-# this rule is trigged to build everything
-all: dirs $(BINDIR)/$(TARGET8XP)
+# Builds everything
+all: $(BINDIR)/$(TARGET8XP)
 
-# this rule is trigged to build debug everything
+# Builds everything with debug flags
 debug: LDDEBUGFLAG = -i 'dbg'
 debug: DEBUGMODE = DEBUG
 debug: CCDEBUGFLAG = -debug
-debug: dirs $(BINDIR)/$(TARGET8XP)
+debug: all
 
+# Creates the bin and obj directories
 dirs:
-	@echo C CE SDK Version $(VERSION) && \
-	$(call MKDIR,$(BINDIR)) && \
+	@$(call MKDIR,$(BINDIR)) && \
 	$(call MKDIR,$(OBJDIR))
 
+# Builds the target 8xp file
 $(BINDIR)/$(TARGET8XP): $(BINDIR)/$(TARGETBIN)
-	@$(CD) $(BINDIR) && \
+	@$(call MKDIR,$(BINDIR)) && \
+	$(CD) $(BINDIR) && \
 	$(CV) $(CVFLAGS) $(notdir $<)
 
+# Builds the target bin file
 $(BINDIR)/$(TARGETBIN): $(LINK_FILES) $(LINK_ICON)
-	@$(LD) $(LDFLAGS) $@
+	@$(call MKDIR,$(BINDIR)) && \
+	$(LD) $(LDFLAGS) $@
 
-# this rule handles conversion of the icon, if it is ever updated
+# Converts the icon png into a source file
 $(OBJDIR)/$(ICON_ASM): $(ICONPNG)
-	@$(ICON_CONV)
+	@$(call MKDIR,$(OBJDIR)) && \
+	$(PG) -c $(ICONPNG)$(comma)$(call NATIVEPATH,$(LINK_ICON))$(comma)$(DESCRIPTION)
 
-# these rules compile the source files into object files
-$(OBJDIR)/%.src: */%.c $(USERHEADERS)
-	@$(call MKDIR,$(call NATIVEPATH,$(@D))) && \
+# Compiles source files into object files
+$(OBJDIR)/%.src: **/%.c $(USERHEADERS) $(TARGETGFX)
+	@$(call MKDIR,$(OBJDIR)) && \
+	$(call MKDIR,$(call NATIVEPATH,$(@D))) && \
 	$(CD) $(call NATIVEPATH,$(@D)) && \
 	$(CC) $(CFLAGS) "$(call WINPATH,$(addprefix $(MAKEDIR)/,$<))"
 
-# these rules compile the source files into object files
-$(OBJDIR)/%.src: **/*/%.c $(USERHEADERS)
-	@$(call MKDIR,$(call NATIVEPATH,$(@D))) && \
-	$(CD) $(call NATIVEPATH,$(@D)) && \
-	$(CC) $(CFLAGS) "$(call WINPATH,$(addprefix $(MAKEDIR)/,$<))"
+# Converts png graphics into source files
+gfx: $(TARGETGFX)
+$(TARGETGFX): $(PNGSOURCES) $(F_CONVPNGINI)
+	@$(CD) $(GFXDIR) && \
+	$(PG)
 
-clean:
-	@$(call RMDIR,$(OBJDIR)) && \
-	$(call RMDIR,$(BINDIR)) && \
+# Cleans generated files
+clean: clean-bin clean-obj clean-gfx
+clean-bin:
+	@$(call RMDIR,$(BINDIR)) && \
 	echo Cleaned build files.
+clean-obj:
+	@$(call RMDIR,$(OBJDIR)) && \
+	echo Cleaned object files.
+clean-gfx:
+	@$(RM) $(GFXSOURCES) && \
+	echo Cleaned generated graphics files.
 
-gfx:
-	@$(CD) $(GFXDIR) && convpng
-
+# Echoes the SDK version
 version:
-	@echo C SDK Version $(VERSION)
+	@echo CE C SDK, version $(VERSION)
 
-.PHONY: all clean version gfx dirs
+.PHONY: all debug dirs gfx clean clean-bin clean-obj clean-gfx version
