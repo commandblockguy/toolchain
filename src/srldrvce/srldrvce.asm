04fc1f5b92a46f16df7652bb990e0130aca86bde
commandblockguy <commandblockguy1@gmail.com>
2019-07-22 22:34:27 -0500

    Not sure why this isn't working

--- src/srldrvce/srldrvce.asm
+++ src/srldrvce/srldrvce.asm
@@ -16,9 +16,6 @@ include_library '../usbdrvce/usbdrvce.asm'
 	export srl_Init
 	export srl_GetType
 	export srl_SetRate
-	export srl_GetRate
-	export srl_SetCoding
-	export srl_GetCoding
 	export srl_Available
 	export srl_Read
 	export srl_Write
@@ -26,6 +23,186 @@ include_library '../usbdrvce/usbdrvce.asm'
 	export srl_Write_Blocking
 ;-------------------------------------------------------------------------------
 
+
+;-------------------------------------------------------------------------------
+; memory structures
+;-------------------------------------------------------------------------------
+macro struct? name*, parameters&
+ macro end?.struct?!
+     iterate base, ., .base
+      if defined base
+       assert base+sizeof base=$
+      end if
+     end iterate
+   end namespace
+  end struc
+  iterate <base,prefix>, 0,, ix-name,x, iy-name,y
+   virtual at base
+	prefix#name	name
+   end virtual
+  end iterate
+  purge end?.struct?
+ end macro
+ struc name parameters
+  namespace .
+end macro
+
+struct srl_Device
+	local size
+	label .: size
+	dev				rl 1	; USB device
+	in				rl 1	; USB bulk in endpoint
+	out				rl 1	; USB bulk out endpoint
+	type			rb 1	; Device type
+	readBuf			rl 1	; Pointer to the read buffer
+	readBufSize		rl 1	; Size of the read buffer
+	readBufStart	rl 1	; First byte with data in the read buffer
+	readBufEnd		rl 1	; Last byte with data in the read buffer
+	readBufBreak	rl 1	; Last byte before the buffer "loops"
+	writeBuf		rl 1	; Pointer to the write buffer
+	writeBufSize	rl 1	; Size of the write buffer
+	writeBufStart	rl 1	; First byte with data in the write buffer
+	writeBufEnd		rl 1	; Last byte with data in the write buffer
+	size := $-.
+end struct
+
+; enum srl_DeviceType
+virtual at 0
+	?SRL_UNKNOWN	equ	0	/**< Incompatible or non-serial device */
+	?SRL_HOST		equ 1	/**< Calc is acting as a device */
+	?SRL_CDC		equ 2	/**< CDC device */
+	?SRL_FTDI		equ 3	/**< FTDI device */
+end virtual
+
+;-------------------------------------------------------------------------------
+;usb_error_t srl_Init(srl_device_t *srl, usb_device_t dev, void *buf, size_t size);
 srl_Init:
+	ld	iy,0
+	add	iy,sp
+;set srl->dev
+
+;determine endpoints
+;determine device type
+;set read buffer pointer, start, and end
+;divide size by 2
+;set buffer sizes
+;set write buffer pointer, start, and end
+;set read buffer break to 0
+;start async read
+	xor	a,a
 	ret
 
+;-------------------------------------------------------------------------------
+;srl_deviceType_t srl_GetType(usb_device_t dev);
+srl_GetType:
+;check if device is host
+;get device descriptors
+;check device class
+;compare device to list of devices
+	ret
+
+;-------------------------------------------------------------------------------
+;usb_error_t srl_SetRate(srl_device_t *srl, uint24_t rate);
+srl_SetRate:
+	ld	iy,0
+	add	iy,sp
+;check device type
+;if a CDC device:
+; change the rate of the default line coding
+; send control request
+; return error, if any
+;if a FTDI device:
+; convert the rate to whatever completely arbitrary format FTDI uses
+; send control request
+; return error, if any
+	ret
+
+;-------------------------------------------------------------------------------
+;size_t srl_Available(srl_device_t *srl);
+srl_Available:
+	ld	iy,0
+	add	iy,sp
+;if readBufBreak == 0:
+; readBufEnd - readBufStart
+;else:
+; (readBufBreak - readBufStart) + (readBufEnd - readBuf)
+	ret
+
+;-------------------------------------------------------------------------------
+;size_t srl_Read(srl_device_t *srl, void *buffer, size_t length);
+srl_Read:
+	ld	iy,0
+	add	iy,sp
+	ret
+
+;-------------------------------------------------------------------------------
+;size_t srl_Write(srl_device_t *srl, const void *buffer, size_t length);
+srl_Write:
+	ld	iy,0
+	add	iy,sp
+;copy data into buffer
+;if writeBufStart == writeBufEnd:
+; schedule transfer
+;update writeBufEnd
+	ret
+
+;-------------------------------------------------------------------------------
+;size_t srl_Read_Blocking(srl_device_t *srl, void *buffer, size_t length, uint24_t timeout);
+srl_Read_Blocking:
+	ld	iy,0
+	add	iy,sp
+;while total < length:
+; check timeout
+; usb_WaitForEvents ; would HandleEvents be better here?
+; srl_Read(srl, buffer + total, total - length)
+; total += len
+	ret
+
+;-------------------------------------------------------------------------------
+;size_t srl_Write_Blocking(srl_device_t *srl, const void *buffer, size_t length, uint24_t timeout);
+srl_Write_Blocking:
+	ld	iy,0
+	add	iy,sp
+;while total < length:
+; check timeout
+; usb_Write(srl, buffer + total, total - length)
+; usb_WaitForEvents ; would HandleEvents be better here?
+; total += len
+	ret
+
+;-------------------------------------------------------------------------------
+;usb_error_t (usb_endpoint_t endpoint, usb_transfer_status_t status, size_t transferred, srl_device_t *data);
+srl_Read_Callback:
+	ld	iy,0
+	add	iy,sp
+;update buffer info
+;if ftdi:
+; transferred -= 2
+; copy data over 2 bytes
+;readBufEnd += transferred
+;if readBufEnd + 64 > readBuf + readBufSize:
+; readBufBreak = readBufEnd
+; readBufEnd = readBuf
+;if readBufBreak:
+; if readBufEnd + 64 <= readBufStart:
+;  reschedule transfer at readBufEnd
+;else:
+; reschedule transfer at readBufEnd
+	ret
+
+;-------------------------------------------------------------------------------
+;usb_error_t (usb_endpoint_t endpoint, usb_transfer_status_t status, size_t transferred, srl_device_t *data);
+srl_Write_Callback:
+	ld	iy,0
+	add	iy,sp
+;update buffer info
+;writeBufStart += transferred
+;if writeBufStart == writeBuf + writeBufSize:
+; writeBufStart = writeBuf
+;if writeBufStart == writeBufEnd:
+; return
+;if writeBufEnd < writeBufStart:
+; reschedule transfer at writeBufStart with size writeBuf+writeBufSize-writeBufSize
+;else:
+; reschedule transfer at writeBufStart with size writeBufEnd - writeBufStart
+	ret
--- src/srldrvce/srldrvce.h
+++ src/srldrvce/srldrvce.h
@@ -19,10 +19,10 @@ extern "C" {
 #endif
 
 enum srl_DeviceType {
-    SRL_HOST = -1,   /**< Calc is acting as a device */
-    SRL_UNKNOWN = 0, /**< Incompatible or non-serial device */
-    SRL_CDC,         /**< CDC device */
-    SRL_FTDI         /**< FTDI device */
+    SRL_UNKNOWN, /**< Incompatible or non-serial device */
+    SRL_HOST,    /**< Calc is acting as a device */
+    SRL_CDC,     /**< CDC device */
+    SRL_FTDI     /**< FTDI device */
 };
 
 typedef uint8_t srl_deviceType_t;
@@ -43,36 +43,16 @@ typedef struct srl_Device {
     char *writeBufEnd;      /**< Last byte with data in the write buffer */
 } srl_device_t;
 
-enum charFormat {
-    SRL_SB_1 = 0,
-    SRL_SB_1_5,
-    SRL_SB_2
-};
-
-enum parityType {
-    SRL_PARITY_NONE = 0,
-    SRL_PARITY_ODD,
-    SRL_PARITY_EVEN,
-    SRL_PARITY_MARK,
-    SRL_PARITY_SPACE
-};
-
-typedef struct srl_LineCoding {
-    uint32_t rate;      /**< Baud rate */
-    uint8_t charFormat; /**< Number of stop bits (generally 1) */
-    uint8_t parityType; /**< Parity type (generally none) */
-    uint8_t dataBits;   /**< Number of data bits (generally 8) */
-} srl_lineCoding_t;
-
 
 /**
  * Initializes a serial device and assigns it a buffer.
  * The buffer must be at least 128 bytes long, but a size of 512 bytes is recommended.
  * @param dev USB device to be used as a serial device.
  * @param buf The buffer's address.
+ * @param size The size of the buffer
  * @return USB_SUCCESS on success, otherwise an error.
  */
-usb_error_t srl_Init(srl_device_t *srl, usb_device_t dev, void *buf);
+usb_error_t srl_Init(srl_device_t *srl, usb_device_t dev, void *buf, size_t size);
 
 /**
  * Get the type of a serial device.
@@ -86,26 +66,6 @@ srl_deviceType_t srl_GetType(usb_device_t dev);
  * @return USB_SUCCESS on success, otherwise an error.
  */
 usb_error_t srl_SetRate(srl_device_t *srl, uint24_t rate);
-
-/**
- * Get the baud rate of the device.
- * @return Baud rate, or 0 on error.
- */
-uint24_t srl_GetRate(srl_device_t *srl);
-
-/**
- * Set the device line coding.
- * @param coding Line coding to use.
- * @return USB_SUCCESS on success, otherwise an error.
- */
-usb_error_t srl_SetCoding(srl_device_t *srl, const srl_lineCoding_t *coding);
-
-/**
- * Get the device's current line coding.
- * @param coding Buffer to store the device's current line coding.
- * @return USB_SUCCESS on success, otherwise an error.
- */
-usb_error_t srl_GetCoding(srl_device_t *srl, srl_lineCoding_t *coding);
 
 /**
  * Get the number of bytes available in the input buffer.
